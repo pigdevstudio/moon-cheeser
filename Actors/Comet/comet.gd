33b75ed0e27f536397595049b8f4a549d80a54e3
Henrique Campos <henriiquecampos@gmail.com>
2017-07-15 19:10:20 -0300

    Added abstract space body class
    also edited some classes to inherit and added the new starmouse sprite

--- Actors/Comet/comet.gd
+++ Actors/Comet/comet.gd
@@ -1,25 +1,7 @@
-extends KinematicBody2D
+extends "res://Actors/abstract_space_body.gd"
 
-var direction = 1
-var is_mouse_on = false
-var already_pressed = false
-
-onready var fixed_process = set_fixed_process(true)
-onready var velocity = Vector2()
-
-func _exit_screen():
-	queue_free()
-
-func _ready():
-	velocity = Vector2(direction,0) * 300
 
 func _fixed_process(delta):
-	if Input.is_mouse_button_pressed(1) and not already_pressed and is_mouse_on:
-		var n = (get_global_mouse_pos() - get_global_pos()).normalized() * Vector2(1, -1)
-		velocity = n.slide(velocity) * 5
-		velocity = Vector2(300 * direction,velocity.y)
-		already_pressed = true
-	var motion = move(velocity * delta)
 	if is_colliding():
 		if get_collider().is_in_group("moon"):
 			var crater = load("res://Objects/Crater/Crater.tscn").instance()
@@ -30,7 +12,4 @@ func _fixed_process(delta):
 			var c = load("res://Objects/Cheese/Cheese.tscn").instance()
 			get_collider().get_node("Sprite").add_child(c)
 			c.set_pos(get_collision_pos())
-			
-			queue_free()
-func _set_mouse_on(is_on):
-	is_mouse_on = is_on
+		queue_free()

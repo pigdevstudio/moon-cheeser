20584c774dae3e42b080b87729f515c2695bef8f
Henrique Campos <henriquecampos@pigdev.studio>
2019-02-06 16:14:58 -0200

    Port game from Godot 2.1 to 3.1

---
+++ source_3.1/Objects/Cheese/cheese.gd
@@ -0,0 +1,44 @@
+#MIT License
+#
+#Copyright (c) 2017 Pigdev Studio
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+extends StaticBody2D
+export var score = 1
+
+func _ready():
+	connect("tree_exited", get_parent(), "count_cheeses")
+
+func float_around(offset):
+	randomize()
+	var t = get_node("Tween")
+	t.interpolate_method(self, "set_position", get_position(), offset, 4, t.TRANS_ELASTIC, t.EASE_OUT)
+	t.start()
+	
+func increase_score():
+	score_handler.set_score(score)
+	acheesements.modify_achievement("mooncheeser", 1)
+	queue_free()
+	
+func move_towards(where):
+	var t = get_node("Tween")
+	var direction = (where - get_global_position()).normalized()
+	var target = get_global_position() + (Vector2(100, 100) * direction)
+	t.interpolate_method(self, "set_global_position", get_global_position(), target, 1.0, t.TRANS_BACK, t.EASE_IN_OUT)
+	t.start()

e66f782327dae9d0f382e33629873ae12be34e6e
Henrique Campos <henriquecampos@pigdev.studio>
2019-02-07 16:57:17 -0200

    Change Physics to use kinematic_actor instead of parent

--- source/actors/Astromouse/physics/Physics.gd
+++ source/actors/Astromouse/physics/Physics.gd
@@ -4,4 +4,10 @@ var kinematic_actor
 
 func _physics_process(delta):
 	for force in get_children():
-		force.apply(get_parent())
+		force.apply(kinematic_actor)
+
+func _on_Astromouse_collided(collision):
+	var collider = collision.collider
+	var angle = kinematic_actor.get_angle_to(collider.global_position)
+	angle -= deg2rad(90)
+	kinematic_actor.rotate(angle)

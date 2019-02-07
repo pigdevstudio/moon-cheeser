be16f1ba7e29161c219048347547c1722db6fcd6
Henrique Campos <henriquecampos@pigdev.studio>
2019-02-07 16:57:56 -0200

    Add interface to access interesting nodes easier

--- source/actors/Astromouse/KinematicActor.gd
+++ source/actors/Astromouse/KinematicActor.gd
@@ -1,7 +1,6 @@
 extends KinematicBody2D
 signal collided(collision)
 
-export (Vector2) var floor_normal = Vector2(0, 1)
 var velocity = Vector2(0, 0)
 
 func _ready():
@@ -11,11 +10,10 @@ func _ready():
 func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
 	if collision:
-		if collision.collider.is_in_group("moon"):
-			#Offset by -90 degrees to make the character always point
-			#up relative to the collider's center
-			rotate(get_angle_to(collision.collider.global_position) - deg2rad(90))
-			emit_signal("collided", collision)
+		emit_signal("collided", collision)
 
 func get_gravity():
 	return $Physics/Gravity
+
+func get_jump():
+	return $Actions/Jump

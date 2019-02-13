be4b53ab27f9873b5098a5426f8076fc35290d22
Henrique Campos <henriquecampos@pigdev.studio>
2019-02-13 15:10:01 -0200

    Add the ability to spawn cheeses when a comet collide with the moon

--- source/levels/level_0-0.gd
+++ source/levels/level_0-0.gd
@@ -3,3 +3,4 @@ extends Node
 func _ready():
 	$Moon.astromouse = $Astromouse
 	randomize()
+
--- source/objects/Moon/Moon.gd
+++ source/objects/Moon/Moon.gd
@@ -5,12 +5,25 @@ var astromouse setget set_astromouse
 func _ready():
 	set_physics_process(false)
 
+
 func _physics_process(delta):
 	var normal = (global_position - astromouse.global_position).normalized()
 	$Gravity.direction = normal
 	$Gravity.apply(astromouse)
-	
+
+
 func set_astromouse(new_astromouse):
 	astromouse = new_astromouse
 	astromouse.connect("tree_exited", self, "set_physics_process", [false])
 	set_physics_process(true)
+
+
+func spawn_cheeses(collision):
+	var spawner = load("res://objects/Spawner/CheeseSpawner.tscn").instance()
+	var pos = collision.position
+	var normal = collision.normal
+	$Pivot.add_child(spawner)
+	spawner.position = $Pivot.to_local(pos)
+	spawner.rotation = normal.angle() + deg2rad(90 - $Pivot.rotation_degrees)
+	spawner.spawn()
+

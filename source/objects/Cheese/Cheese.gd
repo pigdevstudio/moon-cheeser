b36509260d9d402ae729d8ba7091dd19751a7ec3
Henrique Campos <henriquecampos@pigdev.studio>
2019-02-27 11:21:34 -0300

    Close #14

--- source/objects/Cheese/Cheese.gd
+++ source/objects/Cheese/Cheese.gd
@@ -1,10 +1,20 @@
 extends Area2D
 
-func surge():
+signal scored(amount)
+
+export (int) var score = 100
+
+func surge(offset_distance):
 	var t = $Tween
-	var offset = rand_range(50, 200) * Vector2(0, -1).rotated(rotation)
+	var offset = offset_distance * Vector2(0, -1).rotated(rotation)
 	
 	t.interpolate_property(self, "position", position, position + offset, 0.5,
 			t.TRANS_BACK, t.EASE_OUT)
 	t.start()
+
+
+func _on_area_entered(body):
+	if body.is_in_group("player"):
+		emit_signal("scored", score)
+		queue_free()
 

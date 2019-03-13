4cdfb63d6c5880e39968b23ae1a983d266f079be
Henrique Campos <henriquecampos@pigdev.studio>
2019-03-13 13:19:04 -0300

    Close #34
    Also add custom fonts to Title and Button, add margin and organize labels and button in a vertical box container.

--- source/screens/achievements_screen/AchievementPanel.gd
+++ source/screens/achievements_screen/AchievementPanel.gd
@@ -1,7 +1,7 @@
 extends Control
 
-onready var description_label = $Description
-onready var title_label = $Title
+onready var description_label = $MarginContainer/VBoxContainer/Description
+onready var title_label = $MarginContainer/VBoxContainer/Title
 
 func _ready():
 	acheesements.read_achievements()
@@ -11,6 +11,13 @@ func display(achievement):
 	var description = acheesements.dict[achievement].description
 	var accomplished = acheesements.dict[achievement].accomplished
 	var total = acheesements.dict[achievement].total
-	title_label.text = title
-	description_label.text = ("%s, you already accomplished: %s / %s" % [description, accomplished, total])
+	
+	title_label.text = title.to_lower()
+	
+	var description_text = "%s, you already accomplished: %s / %s"
+	description_text = description_text % [description, accomplished,
+			total]
+	description_text = description_text.to_lower()
+	description_label.text = description_text
+	
 	show()

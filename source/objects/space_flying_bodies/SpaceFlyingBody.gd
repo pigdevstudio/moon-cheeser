2b956e718716a9c14bdca40ae3c9dcd53778836f
Henrique Campos <henriquecampos@pigdev.studio>
2019-03-17 13:37:20 -0300

    Merge FlyingBody and SpaceFlyingBody classes

--- source/objects/space_flying_bodies/SpaceFlyingBody.gd
+++ source/objects/space_flying_bodies/SpaceFlyingBody.gd
@@ -1,9 +1,15 @@
-extends "res://objects/space_flying_bodies/FlyingBody.gd"
+extends "res://objects/SpaceKinematicBody.gd"
+
+"""
+A flying body that fly towards a given speed at a given direction
+"""
+
+export (float) var flying_speed = 300.0
 
 func _on_collided(collision):
 	var collider = collision.collider
 	if collider.has_method("spawn_cheeses"):
-		collider.spawn_cheeses(collision)
+		collider.spawn_cheeses(collision.position)
 	if collider.has_method("spawn_crater"):
 		collider.spawn_crater(collision)
 	queue_free()
@@ -12,11 +18,15 @@ func _on_DragArea_dragged(direction):
 	$Shape.disabled = false
 	$DragArea/Shape.disabled = true
 
+func fly(direction):
+	if direction.length() > 0:
+		velocity = flying_speed * direction
+		rotation = direction.angle()
 
-func spawn_cheeses(collision):
+func spawn_cheeses(spawn_position):
 	var spawner = get_node("CheeseSpawner").duplicate()
 	get_parent().add_child(spawner)
-	spawner.position = collision.position
+	spawner.position = spawn_position
 	spawner.spawn()
 	queue_free()
 

d78d6cf6607bfd8a56a98cd0878b86e2e4252a83
Henrique Campos <henriquecampos@pigdev.studio>
2019-03-19 14:47:00 -0300

    Add FirstSteps achievement trigger

--- source/meta/achievements/FirstStepsAchievement.gd
+++ source/meta/achievements/FirstStepsAchievement.gd
@@ -1,7 +1,12 @@
 extends "res://meta/achievements/AchievementTrigger.gd"
 
-func _on_Comet_collided(collision):
-	var collider = collision.collider
-	
-	if collider.is_in_group("moon"):
+var _dragged = false
+var _moon_collided = false
+
+func _on_DragArea_dragging():
+	_dragged = true
+
+func _on_KillingArea_body_entered(body):
+	_moon_collided = body.is_in_group("moon")
+	if _dragged and _moon_collided:
 		increase_achievement_progress()

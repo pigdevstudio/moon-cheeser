cbb856e3fa99cc23fc915941694f336002e39844
Henrique Campos <henriquecampos@pigdev.studio>
2019-03-19 14:44:39 -0300

    Add invincibility to Astromouse

--- source/levels/Level.gd
+++ source/levels/Level.gd
@@ -13,35 +13,26 @@ func _ready():
 	get_tree().connect("node_added", self, "_on_SceneTree_node_added")
 	randomize()
 
-
 func set_astromouse(new_astromouse):
-	if not new_astromouse:
-		return
-	if not new_astromouse is Astromouse:
-		return 
 	astromouse = new_astromouse
+	astromouse.become_invincible()
 	astromouse.connect("died", self, "_on_Astromouse_died")
 	$Moon.astromouse = astromouse
 	if has_node("Blackhole"):
 		get_node("Blackhole").astromouse = astromouse
 
-
 func _on_Astromouse_died():
  	emit_signal("game_over")
-
 
 func _on_Starmouse_tree_entered(starmouse):
 	astromouse.queue_free()
 	starmouse.connect("tree_exiting", self, "_on_Starmouse_tree_exiting", [starmouse])
 
-
 func _on_Starmouse_tree_exiting(starmouse):
 	instance_astromouse(starmouse.position)
 
-
 func _on_SceneTree_node_added(node):
 	treat_new_node(node)
-
 
 func _on_Blackhole_tree_entered(blackhole):
 	blackhole.connect("tree_exited", self, "_on_Blackhole_tree_exited")
@@ -52,24 +43,21 @@ func _on_Blackhole_tree_entered(blackhole):
 	get_tree().call_group("flying_body", "queue_free")
 	get_tree().call_group("crater", "queue_free")
 
-
 func _on_Blackhole_tree_exited():
 	if is_inside_tree():
 		get_tree().call_group("flyingbody_spawner", "reset_timer")
 		$Moon.set_physics_process(true)
 		$Moon.set_process_unhandled_input(false)
 
-
 func instance_astromouse(spawn_position):
 	var spawner = $AstromouseSpawner
 	spawner.position = spawn_position
 	spawner.spawn()
 
-
 func treat_new_node(node):
 	if node is Starmouse:
 		_on_Starmouse_tree_entered(node)
-	elif node is Astromouse:
+	elif node.has_node("Actions"):
 		set_astromouse(node)
 	elif node is Blackhole:
 		_on_Blackhole_tree_entered(node)

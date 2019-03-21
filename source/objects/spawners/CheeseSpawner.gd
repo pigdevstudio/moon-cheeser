74897701ff3abff6c9b668f3f02f78ca64c05ffe
Henrique Campos <henriquecampos@pigdev.studio>
2019-03-21 10:18:32 -0300

    Move "surge" responsibility to CheeseSpawner

--- source/objects/spawners/CheeseSpawner.gd
+++ source/objects/spawners/CheeseSpawner.gd
@@ -1,5 +1,9 @@
 extends "res://objects/spawners/Spawner.gd"
 
+"""
+Spread a random amount of cheeses 
+"""
+
 export (int) var min_cheeses = 2
 export (int) var max_cheeses = 8
 
@@ -13,9 +17,17 @@ func spawn():
 		var cheese = spawnling.instance()
 		add_child(cheese)
 		cheese.rotation_degrees = rand_range(-spread, spread)
-		cheese.surge(rand_range(min_radius, max_radius))
 		cheese.connect("scored", Score, "add_score")
 		cheese.connect("tree_exited", self, "_on_cheese_tree_exited")
+		surge(cheese, rand_range(min_radius, max_radius))
+
+func surge(cheese, offset_distance):
+	var tween = $Tween
+	var offset = offset_distance * Vector2(0, -1).rotated(cheese.rotation)
+	
+	tween.interpolate_property(cheese, "position", cheese.position, 
+			cheese.position + offset, 0.5, tween.TRANS_BACK, tween.EASE_OUT)
+	tween.start()
 
 func set_spread(value):
 	spread = clamp(value, 0, 180)

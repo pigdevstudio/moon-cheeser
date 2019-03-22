ded25193f864fee23910ac2707fa2e4d01dfcea2
Henrique Campos <henriquecampos@pigdev.studio>
2019-03-22 14:25:34 -0300

    Remove SwingPivot get_angle_quadrant
    There is only 1 corner case for get_rest_angle not being, which is when the movement is lelt-down

--- source/actors/starmouse/SwingPivot.gd
+++ source/actors/starmouse/SwingPivot.gd
@@ -5,7 +5,7 @@ Swing following a movement direction to give an inertia aspect
 """
 
 export (float) var angle_offset_degrees = 90
-export (float, 1.0) var dampling = 0.02
+export (float, 1.0) var damping = 0.02
 
 var movement_velocity = Vector2(0, 0)
 var is_swinging = false
@@ -25,7 +25,7 @@ func rest():
 func damp_to_rest_angle():
 	var rest_angle_degrees = get_rest_angle_degrees()
 	while not is_swinging:
-		rotation_degrees = lerp(rotation_degrees, rest_angle_degrees, dampling)
+		rotation_degrees = lerp(rotation_degrees, rest_angle_degrees, damping)
 		yield(get_tree(), "physics_frame")
 
 func flip_horizontally():
@@ -36,18 +36,8 @@ func flip_horizontally():
 		scale.x = 1
 
 func get_rest_angle_degrees():
-	var rest_angle = 0.0
+	var is_movement_bottom_left = movement_velocity.y > 0 and movement_velocity.x < 0 
 	
-	var quadrant = get_angle_quadrant()
-	if quadrant == 2 or quadrant == 4:
-		rest_angle = 360.0
-	
+	var rest_angle = 0.0 if not is_movement_bottom_left else 360.0
 	return rest_angle
-
-func get_angle_quadrant():
-	var angle_quadrant = 1
 	
-	var angle = rotation_degrees - angle_offset_degrees
-	
-	angle_quadrant = int(ceil(angle / 90))
-	return angle_quadrant

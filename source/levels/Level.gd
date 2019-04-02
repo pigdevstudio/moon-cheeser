4f5c2b8e778281301d457471a068f56bb41e30c5
Henrique Campos <henriquecampos@pigdev.studio>
2019-04-02 15:00:00 -0300

    Add hooks for achievements rewards

--- source/levels/Level.gd
+++ source/levels/Level.gd
@@ -3,6 +3,7 @@ extends Node
 signal game_over
 
 var astromouse setget set_astromouse
+var has_astromouse = false
 
 func _ready():
 	set_astromouse($Astromouse)
@@ -13,27 +14,40 @@ func set_astromouse(new_astromouse):
 	astromouse = new_astromouse
 	astromouse.become_invincible()
 	astromouse.connect("died", self, "_on_Astromouse_died")
-	
+	astromouse.connect("tree_exited", self, "_on_Astromouse_tree_exited")
+	has_astromouse = true
+
 func _on_Astromouse_tree_entered(new_astromouse):
 	set_astromouse(new_astromouse)
 	$Moon.astromouse = astromouse
-	
+
+func _on_Astromouse_tree_exited():
+	has_astromouse = false
+
 func _on_Astromouse_died():
  	emit_signal("game_over")
 
 func _on_Starmouse_tree_entered(starmouse):
+	if Achievements.is_achievement_complete("starmouse"):
+		$Background.show_crux()
+		BGM.play_track("Harmony")
 	astromouse.queue_free()
 	starmouse.connect("tree_exiting", self, "_on_Starmouse_tree_exiting", [starmouse])
 
 func _on_Starmouse_tree_exiting(starmouse):
+	if Achievements.is_achievement_complete("starmouse"):
+		$Background.hide_crux()
+		BGM.stop_track("Harmony")
 	instance_astromouse(starmouse.position)
 
 func _on_SceneTree_node_added(node):
 	treat_new_node(node)
 
 func _on_Blackhole_tree_entered(blackhole):
+	if Achievements.is_achievement_complete("gravity"):
+		BGM.play_track("Pad")
 	blackhole.connect("tree_exited", self, "_on_Blackhole_tree_exited")
-	if astromouse:
+	if has_astromouse:
 		blackhole.astromouse = astromouse
 		var astromouse_actions = astromouse.get_action_node()
 		astromouse_actions.set_process_unhandled_input(false)
@@ -45,11 +59,14 @@ func _on_Blackhole_tree_entered(blackhole):
 
 func _on_Blackhole_tree_exited():
 	if is_inside_tree():
+		if Achievements.is_achievement_complete("gravity"):
+			BGM.stop_track("Pad")
 		get_tree().call_group("flyingbody_spawner", "reset_timer")
 		$Moon.set_physics_process(true)
 		$Moon.set_process_unhandled_input(false)
-		var astromouse_actions = astromouse.get_action_node()
-		astromouse_actions.set_process_unhandled_input(true)
+		if has_astromouse:
+			var astromouse_actions = astromouse.get_action_node()
+			astromouse_actions.set_process_unhandled_input(true)
 
 func instance_astromouse(spawn_position):
 	var spawner = $AstromouseSpawner

92cdaa1ee43e0701d0dab802b3a6d77840bea80e
Henrique Campos <henriquecampos@pigdev.studio>
2019-04-02 14:55:38 -0300

    Add new background music tracks and moved files

---
+++ source/screens/background_music/BackgroundMusic.gd
@@ -0,0 +1,16 @@
+extends Node
+
+onready var animator = $AnimationPlayer
+
+func play_track(track = ""):
+	if has_node(track):
+		animator.play(track)
+		track = get_node(track)
+		track.play($Bass.get_playback_position())
+		
+func stop_track(track = ""):
+	if has_node(track):
+		animator.play_backwards(track)
+		track = get_node(track)
+		yield(animator, "animation_finished")
+		track.stop()
